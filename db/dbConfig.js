840947a9fe5dc8c0143ad1417ac5078e5511d720
Renan.T <rtoniolo4@gmail.com>
2023-11-06 22:43:07 -0300

    adjust

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -4,10 +4,7 @@ mongoose.connect(process.env.MONGODB_URI);
 
 let db = mongoose.connection;
 
-db.on(
-  "error",
-  console.log.bind(console, `Erro de conexão ${process.env.MONGODB_URI}`)
-);
+db.on("error", console.log.bind(console, `Erro de conexão`));
 db.once("open", () => {
   console.log("Connect dataBase: sucessfully");
 });
